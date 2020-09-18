c16ecc98a93ad2490adbee5d6f89a4840a4d60f6
shengrongchun325 <shengrongchun325@zto.com>
2020-09-18 19:42:22 +0800

    stage-5: router-view缓存初始化

--- router/components/view.js
+++ router/components/view.js
@@ -17,27 +17,62 @@ export default {
     const h = parent.$createElement
     const name = props.name
     const route = parent.$route //组件依赖了$route
+    //创建缓存
+    const cache = parent._routerViewCache || (parent._routerViewCache = {})
+
     // determine current view depth, also check to see if the tree
     // has been toggled inactive but kept-alive.
     let depth = 0 //自己是第几层的 router-view
+    let inactive = false //激活标识
     while (parent && parent._routerRoot !== parent) {//有parent并且parent不是根实例
       const vnodeData = parent.$vnode ? parent.$vnode.data : {}
       if (vnodeData.routerView) {//router-view组件标识
         depth++//一直向上级父层查找,找到 depth加一
       }
+      //上层是有keep-alive包裹并且是非激活状态
+      if (vnodeData.keepAlive && parent._directInactive && parent._inactive) {
+        inactive = true
+      }
       parent = parent.$parent
     }
     data.routerViewDepth = depth
     //
+
+    // render previous view if the tree is inactive and kept-alive
+    if (inactive) {//true的意思是此router-view往上遍历有keep-alive包裹，并且当前是非激活状态
+      const cachedData = cache[name]
+      const cachedComponent = cachedData && cachedData.component
+      if (cachedComponent) {
+        // #2301
+        // pass props
+        if (cachedData.configProps) {
+          fillPropsinData(cachedComponent, data, cachedData.route, cachedData.configProps)
+        }
+        return h(cachedComponent, data, children)
+      } else {
+        // render previous empty view
+        return h()
+      }
+    }
+
+    //
     const matched = route.matched[depth] //record
     const component = matched && matched.components[name]
     if (!matched || !component) {
+      cache[name] = null //缓存为null
       return h()
     }
+    // cache component 设置缓存
+    cache[name] = { component }
+
     // 从record中获取此组件name的props
     const configProps = matched.props && matched.props[name]
     // save route and configProps in cache
     if (configProps) {//把props信息注入到组件的props中
+      extend(cache[name], {
+        route,
+        configProps
+      })
       fillPropsinData(component, data, route, configProps)
     }
     return h(component, data, children)
@@ -48,7 +83,6 @@ function fillPropsinData(component, data, route, configProps) {
   // data.props在组件render过程中，会给相同key的component.props赋值
   // 如：data.props = {name:111},组件的props如果定义了name,name的值会赋值为111
   let propsToPass = data.props = resolveProps(route, configProps)
-  console.log('propsToPass', propsToPass)
   if (propsToPass) {
     // clone to prevent mutation
     propsToPass = data.props = extend({}, propsToPass)
